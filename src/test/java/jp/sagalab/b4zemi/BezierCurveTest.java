ee845430d06e73a2d2fa2c32a8fd396fcd84d279
inagaki <19026018@mmm.muroran-it.ac.jp>
2023-05-08 00:15:58 +0900

    first commit for B4Zemi2023_Bezier

---
+++ src/test/java/jp/sagalab/b4zemi/BezierCurveTest.java
@@ -0,0 +1,55 @@
+package jp.sagalab.b4zemi;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class BezierCurveTest {
+
+  @Test
+  public void testGetControlPoints() {
+    System.out.println("GetControlPoints");
+    BezierCurve b = BezierCurve.create(Arrays.asList(
+            Point.create(-2.0, 0.0), Point.create(-1.0, 0.0),Point.create(0.0, 2.0), Point.create(1.0, 0.0), Point.create(2.0, 0.0)));
+    assertEquals(-2.0, b.getControlPoints().get(0).getX(), 1.0e-10);
+    assertEquals(-1.0, b.getControlPoints().get(1).getX(), 1.0e-10);
+    assertEquals(0.0, b.getControlPoints().get(2).getX(), 1.0e-10);
+    assertEquals(1.0, b.getControlPoints().get(3).getX(), 1.0e-10);
+    assertEquals(2.0, b.getControlPoints().get(4).getX(), 1.0e-10);
+    assertEquals(0.0, b.getControlPoints().get(0).getY(), 1.0e-10);
+    assertEquals(0.0, b.getControlPoints().get(1).getY(), 1.0e-10);
+    assertEquals(2.0, b.getControlPoints().get(2).getY(), 1.0e-10);
+    assertEquals(0.0, b.getControlPoints().get(3).getY(), 1.0e-10);
+    assertEquals(0.0, b.getControlPoints().get(4).getY(), 1.0e-10);
+    assertEquals(5, b.getControlPoints().size());
+  }
+
+  @Test
+  public void testGetDegree() {
+    System.out.println("GetControlPoints");
+    BezierCurve b = BezierCurve.create(Arrays.asList(
+            Point.create(-2.0, 0.0), Point.create(-1.0, 0.0), Point.create(0.0, 2.0), Point.create(1.0, 0.0), Point.create(2.0, 0.0)));
+
+    assertEquals(4, b.getDegree());
+  }
+
+  @Test
+  public void testEvaluate() {
+    System.out.println("Evaluate");
+    BezierCurve b = BezierCurve.create(Arrays.asList(
+            Point.create(-2.0, 0.0), Point.create(-1.0, 0.0), Point.create(0.0, 2.0), Point.create(1.0, 0.0), Point.create(2.0, 0.0)));
+
+    assertEquals(-2.0, b.evaluate(0.0).getX(), 1.0e-10);
+    assertEquals(-1.0, b.evaluate(0.25).getX(), 1.0e-10);
+    assertEquals(0.0, b.evaluate(0.5).getX(), 1.0e-10);
+    assertEquals(1.0, b.evaluate(0.75).getX(), 1.0e-10);
+    assertEquals(2.0, b.evaluate(1.0).getX(), 1.0e-10);
+    assertEquals(0.0, b.evaluate(0.0).getY(), 1.0e-10);
+    assertEquals(27 / 64.0, b.evaluate(0.25).getY(), 1.0e-10);
+    assertEquals(0.75, b.evaluate(0.5).getY(), 1.0e-10);
+    assertEquals(27 / 64.0, b.evaluate(0.75).getY(), 1.0e-10);
+    assertEquals(0.0, b.evaluate(1.0).getY(), 1.0e-10);
+  }
+}
---
+++ src/test/java/jp/sagalab/b4zemi/PointTest.java
@@ -0,0 +1,54 @@
+package jp.sagalab.b4zemi;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by jumpaku on 2017/05/23.
+ */
+public class PointTest {
+  @Test
+  public void testGetX() {
+    System.out.println("GetX");
+
+    Point instance1 = Point.create(2.0, 4.3);
+    assertEquals(2.0, instance1.getX(), 1.0e-10);
+
+    Point instance2 = Point.create(-0.04, 6.3);
+    assertEquals(-0.04, instance2.getX(), 1.0e-10);
+  }
+
+  @Test
+  public void testGetY() {
+    System.out.println("GetY");
+
+    Point instance1 = Point.create(2.0, 4.3);
+    assertEquals(4.3, instance1.getY(), 1.0e-10);
+
+    Point instance2 = Point.create(-0.04, 6.3);
+    assertEquals(6.3, instance2.getY(), 1.0e-10);
+  }
+
+  @Test
+  public void testDivide() {
+    System.out.println("Divide");
+
+    Point p0 = Point.create(10.0, 1.0);
+    Point p2 = Point.create(20.0, 2.0);
+
+    assertEquals(13.0, p0.divide(p2, 0.3).getX(), 1.0e-10);
+    assertEquals(0.0, p0.divide(p2, -1.0).getX(), 1.0e-10);
+    assertEquals(30.0, p0.divide(p2, 2.0).getX(), 1.0e-10);
+    assertEquals(10.0, p0.divide(p2, 0.0).getX(), 1.0e-10);
+    assertEquals(20.0, p0.divide(p2, 1.0).getX(), 1.0e-10);
+
+    assertEquals(1.3, p0.divide(p2, 0.3).getY(), 1.0e-10);
+    assertEquals(0.0, p0.divide(p2, -1.0).getY(), 1.0e-10);
+    assertEquals(3.0, p0.divide(p2, 2.0).getY(), 1.0e-10);
+    assertEquals(1.0, p0.divide(p2, 0.0).getY(), 1.0e-10);
+    assertEquals(2.0, p0.divide(p2, 1.0).getY(), 1.0e-10);
+  }
+
+}
