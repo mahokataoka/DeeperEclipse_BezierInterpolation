e59b743ec2dc07165f1a14aae1bb52d2c60b9248
akaneyusa <20026244@mmm.muroran-it.ac.jp>
2023-05-09 18:54:29 +0900

    first commit

---
+++ src/main/java/jp/sagalab/b4zemi/Point.java
@@ -0,0 +1,37 @@
+package jp.sagalab.b4zemi;
+
+public class Point {
+    //フィールド(変数)
+    private final double m_x;
+    private final double m_y;
+    //コンストラクタ(初期化)
+    private Point(double _x,double _y){
+        m_x = _x;
+        m_y = _y;
+    }
+    //点の生成を行うためのstaticファクトリーメソッド
+    static Point create(double _x,double _y){
+        return new Point(_x,_y);
+    }
+    //点のx座標を取得する
+    public double getX(){
+        return m_x;
+    }
+    //点のy座標を取得する．
+    public double getY(){
+        return m_y;
+    }
+    //この点ともう一つの点を t : ( 1 - t ) で内分 ( t < 0 または t > 1 の場合は外分 ) する.
+    public Point divide(Point _other,double t){
+        double px = getX();
+        double py = getY();
+        double qx = _other.getX();
+        double qy = _other.getY();
+
+        double divx = ((1-t) * px + t * qx);
+        double divy = ((1-t) * py + t * qy);
+
+        return create(divx,divy);
+    }
+
+}
