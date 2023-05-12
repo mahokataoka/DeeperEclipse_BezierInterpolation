e7dbb123956ce0c30741de71f5942ff7a7c6c39c
inagaki <19026018@mmm.muroran-it.ac.jp>
2023-05-12 23:47:16 +0900

    implemented Drawer class and add javadocs

--- src/main/java/jp/sagalab/b4zemi/Drawer.java
+++ src/main/java/jp/sagalab/b4zemi/Drawer.java
@@ -7,7 +7,14 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 
+/**
+ * 描画用のパネルを生成します．
+ */
 public class Drawer extends JPanel {
+
+  /**
+   * 描画用のパネルの生成を行うためのstaticファクトリーメソッドです．
+   */
   public static void create() {
     if (WIDTH_SIZE <= 0) {
       throw new IllegalArgumentException("WIDTH_SIZE must be grater than 0");
@@ -30,6 +37,13 @@ public class Drawer extends JPanel {
     frame.setVisible(true);
   }
 
+  /**
+   * 指定した座標を指定した色でパネルに描画します．
+   * @param _x     点のx座標
+   * @param _y     点のy座標
+   * @param _color 色
+   * @param _g     グラフィックス
+   */
   public void drawPoint(double _x, double _y, Color _color, Graphics _g) {
     double radius = POINT_RADIUS;
     _g.setColor(_color);
@@ -42,6 +56,15 @@ public class Drawer extends JPanel {
     }
   }
 
+  /**
+   * 指定した２つの点の座標を結ぶ線分を指定した色で描画します．
+   * @param _x1    1点目のx座標
+   * @param _y1    1点目のy座標
+   * @param _x2    2点目のx座標
+   * @param _y2    2点目のy座標
+   * @param _color 色
+   * @param _g     グラフィックス
+   */
   public void drawLine(double _x1, double _y1, double _x2, double _y2, Color _color, Graphics _g) {
     _g.setColor(_color);
     Graphics2D g2D = _g instanceof Graphics2D ? ((Graphics2D) _g) : null;
@@ -52,14 +75,9 @@ public class Drawer extends JPanel {
     }
   }
 
-  public int getWidth() {
-    return WIDTH_SIZE;
-  }
-
-  public int getHeight() {
-    return HEIGHT_SIZE;
-  }
-
+  /**
+   * 描画用のパネルの生成をする．
+   */
   public Drawer() {
     this.setPreferredSize(new Dimension(WIDTH_SIZE, HEIGHT_SIZE));
     this.setBackground(Color.WHITE);
@@ -72,9 +90,13 @@ public class Drawer extends JPanel {
     });
   }
 
+  /** 描画パネルの横幅 */
   private static final int WIDTH_SIZE = 800;
+  /** 描画パネルの縦幅 */
   private static final int HEIGHT_SIZE = 600;
+  /** 描画する際の線の幅 */
   private static final float STROKE_WIDTH = 1.5f;
+  /** 点を描画する際の半径 */
   private static final double POINT_RADIUS = 3.0;
 
 }
